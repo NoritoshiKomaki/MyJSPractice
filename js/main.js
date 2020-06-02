850e1c01820f9a4f65105d23f310cf958dff0d0b
NoritoshiKomaki <kmk.non.nrts@gmail.com>
2020-06-02 14:52:19 +0900

    forEach and map

--- js/main.js
+++ js/main.js
@@ -2,20 +2,15 @@
 
 {
   const scores = [80, 90, 40, 70];
+  
+  // 配列の中身を繰り返し処理する
+  scores.forEach((score, index) => {
+    console.log(`Score ${index}: ${score}`)
+  });
 
-  // const [a, b, c, d] = scores;
-  // console.log(a)
-  // console.log(b)
-  // console.log(c)
-  // console.log(d)
+  const prices = [180, 190, 200]
 
-  const [a, b, ...others] = scores
-  console.log(a);
-  console.log(b);
-  console.log(others);
-
-  let x = 30;
-  let y = 70;
-  [x, y] = [y, x];
-  console.log(x, y)
+  // 配列の内容を書き換える
+  const updatedPrices = prices.map(price => price + 20);
+  console.log(updatedPrices);
 }
