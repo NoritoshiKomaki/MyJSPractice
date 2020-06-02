78b5d52787d74bb35650f0ded5354ae380820307
NoritoshiKomaki <kmk.non.nrts@gmail.com>
2020-06-02 14:58:56 +0900

    filter

--- js/main.js
+++ js/main.js
@@ -1,16 +1,8 @@
 'use strict';
 
 {
-  const scores = [80, 90, 40, 70];
-  
-  // 配列の中身を繰り返し処理する
-  scores.forEach((score, index) => {
-    console.log(`Score ${index}: ${score}`)
-  });
+  const numbers = [1, 4, 7, 8, 10];
 
-  const prices = [180, 190, 200]
-
-  // 配列の内容を書き換える
-  const updatedPrices = prices.map(price => price + 20);
-  console.log(updatedPrices);
+  const evenNumbers = numbers.filter(number => number % 2 === 0);
+  console.log(evenNumbers)
 }
