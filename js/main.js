b949a0029e87950811f50e139bc455cdcbdb6952
NoritoshiKomaki <kmk.non.nrts@gmail.com>
2020-06-02 14:29:12 +0900

    array

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
 'use strict';
 
-console.log('Hello World from main.js!')
+{
+  const scores = [80, 90, 40, 70];
+
+  // console.log(`Score: ${scores[0]}`)
+  // console.log(`Score: ${scores[1]}`)
+  // console.log(`Score: ${scores[2]}`)
+
+  for (let i = 0; i < scores.length; i++) {
+    console.log(`Score ${i}: ${scores[i]}`);
+  }
+}
