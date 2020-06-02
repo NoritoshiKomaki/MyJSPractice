7e0d1a5e391bc9c0c549f736cf1834e833fa2c54
NoritoshiKomaki <kmk.non.nrts@gmail.com>
2020-06-02 21:29:43 +0900

    class

--- js/main.js
+++ js/main.js
@@ -1,27 +1,36 @@
 'use strict';
 
 {
-  // let i = 0;
-  // function showTime() {
-  //   console.log(new Date());
-  //   i++;
-  //   if (i > 2) {
-  //     clearInterval(intervalId);
-  //   }
-  // }
+  class Profile {
+    constructor(name, age) {
+      this.name = name;
+      this.age = age;
+    }
 
-  // const intervalId = setInterval(showTime, 1000);
-
-  let i = 0;
-  function showTime() {
-    console.log(new Date());
-    const timeoutId = setTimeout(showTime, 1000);
-    i++;
-    if (i > 2) {
-      clearTimeout(timeoutId);
+    show() {
+      console.log(`私の名前は${this.name}です。年齢は${this.age}歳です`);
     }
   }
 
-  showTime();
+  class Profile2 extends Profile {
+    constructor(name, age, year) {
+      super(name, age)
+      this.year = year
+    }
 
+    show() {
+      super.show();
+      console.log(`${this.year}年前は${this.age - this.year}歳でした。`)
+    }
+  }
+
+  const profiles = [
+    new Profile('小牧', 31),
+    new Profile('丸山', 29),
+    new Profile2('丸山', 29, 5),
+  ]
+
+  profiles[0].show();
+  profiles[1].show();
+  profiles[2].show();
 }
