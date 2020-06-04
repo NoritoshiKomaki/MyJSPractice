16cd6a6bfe0e3f5f48170a6fd10f306462afa178
NoritoshiKomaki <kmk.non.nrts@gmail.com>
2020-06-04 17:55:42 +0900

    dom

--- js/main.js
+++ js/main.js
@@ -1,36 +1,13 @@
 'use strict';
 
 {
-  class Profile {
-    constructor(name, age) {
-      this.name = name;
-      this.age = age;
-    }
+  document.querySelector('button').addEventListener('click', () => {
+    const li = document.createElement('li');
+    const text = document.querySelector('input');
+    li.textContent = text.value;
+    document.querySelector('ul').appendChild(li);
 
-    show() {
-      console.log(`私の名前は${this.name}です。年齢は${this.age}歳です`);
-    }
-  }
-
-  class Profile2 extends Profile {
-    constructor(name, age, year) {
-      super(name, age)
-      this.year = year
-    }
-
-    show() {
-      super.show();
-      console.log(`${this.year}年前は${this.age - this.year}歳でした。`)
-    }
-  }
-
-  const profiles = [
-    new Profile('小牧', 31),
-    new Profile('丸山', 29),
-    new Profile2('丸山', 29, 5),
-  ]
-
-  profiles[0].show();
-  profiles[1].show();
-  profiles[2].show();
+    text.value = '';
+    text.focus();
+  });
 }
