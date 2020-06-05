4f19a3d1aa515af12bd1e4c4cd1c1be5e6215bcf
NoritoshiKomaki <kmk.non.nrts@gmail.com>
2020-06-05 11:04:33 +0900

    omikuji

--- js/main.js
+++ js/main.js
@@ -1,9 +1,18 @@
 'use strict';
 
 {
-  document.querySelector('ul').addEventListener('click', e => {
-    if (e.target.nodeName === 'LI') {
-      e.target.classList.toggle('done');
+  const btn = document.getElementById('btn');
+  
+  btn.addEventListener('click', () => {
+    // const results = ['大吉', '中吉', '凶', '末吉']
+    // btn.textContent = results[Math.floor(Math.random() * results.length)];
+    const n = Math.random();
+    if (n < 0.05) {
+      btn.textContent = '大吉';
+    } else if (n < 0.2) {
+      btn.textContent = '中吉';
+    } else {
+      btn.textContent = '凶';
     }
   });
 }
